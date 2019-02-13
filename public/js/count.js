253113d56b9cc2e7555bab1c0b18ed16f50f1945
igneel73 <igneel.kush73@gmail.com>
2019-02-13 14:55:55 -0800

    added dummy clock

--- public/js/count.js
+++ public/js/count.js
@@ -1,7 +1,16 @@
+
+// vars for time
+var secs = 0;
+var mins = 0;
+var hrs = 0;
+
 // hide popup at the start
+// start the clock too
 $(document).ready(function (e){
 	$(".pop-up").hide();
 });
+
+var clock = setInterval(tick, 1000);
 
 // increase distraction counter by one
 $(".distracted").click(function(e) {
@@ -17,8 +26,10 @@ $(".pause").click(function(e) {
 	var text = $(this).text();
 	if(text.includes("Pause")){
 		$(this).text("Start");
+		clearInterval(clock);
 	} else {
 		$(this).text("Pause");
+		clock = setInterval(tick,1000);
 	}
 })
 
@@ -33,3 +44,32 @@ $(".cancel").click(function(e){
 	// hide the popup
 	$(".pop-up").hide();
 })
+
+// update clock
+function tick(){
+	secs++;
+	mins = parseInt($("#mins").text());
+	hrs = parseInt($("#hours").text());
+
+	if(secs==60){
+		mins++;
+		secs = 0;
+	}
+	else if(mins==60){
+		hrs++;
+		mins = 0;
+	}
+
+	if(hrs<10){
+		$("#hours").text("0" + hrs);
+	} else {
+		$("#hours").text(hrs);
+	}
+
+	if(mins<10){
+		$("#mins").text("0" + mins);
+	} else {
+		$("#mins").text(mins);
+	}
+
+}
