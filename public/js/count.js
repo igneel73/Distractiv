135a08fe1c12665847d82b77c2c5ba807927eda4
igneel73 <igneel.kush73@gmail.com>
2019-02-13 14:20:38 -0800

    added dummy distraction popup

--- public/js/count.js
+++ public/js/count.js
@@ -1,16 +1,35 @@
+// hide popup at the start
+$(document).ready(function (e){
+	$(".pop-up").hide();
+});
+
 // increase distraction counter by one
-$(".distracted").click(function(req,res) {
+$(".distracted").click(function(e) {
 	var count = parseInt($(".dist-num .num").text());
 	count+= 1;
 	$(".dist-num .num").text(count);
+	$(".pop-up").show();
+
 });
 
 
-$(".pause").click(function(req,res) {
+$(".pause").click(function(e) {
 	var text = $(this).text();
 	if(text.includes("Pause")){
 		$(this).text("Start");
 	} else {
 		$(this).text("Pause");
 	}
+})
+
+$(".ok").click(function(e){
+	// save the entry
+	// hide the popup
+	$(".pop-up").hide();
+});
+
+$(".cancel").click(function(e){
+	// don't save the entry
+	// hide the popup
+	$(".pop-up").hide();
 })
