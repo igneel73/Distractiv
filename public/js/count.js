ed3bd10b5c4ad1a01f0f83ae73d9f20f244d6a5c
Andrew Muro <muro_andrew@yahoo.com>
2019-02-13 16:32:08 -0800

    Data Page Progress

--- public/js/count.js
+++ public/js/count.js
@@ -5,7 +5,7 @@ $(".distracted").click(function(req,res) {
 	$(".dist-num .num").text(count);
 });
 
-
+//Start, Pause
 $(".pause").click(function(req,res) {
 	var text = $(this).text();
 	if(text.includes("Pause")){
@@ -13,4 +13,17 @@ $(".pause").click(function(req,res) {
 	} else {
 		$(this).text("Pause");
 	}
-})
+});
+
+//Change Dropdown text to match selected text
+$("#Type .dropdown .dropdown-menu li").click(function(req,res) {
+	$("#Type .dropdown .btn").text($(this).text());
+});
+
+//Change Dropdown text to match selected text for Graph and correct graph output
+$("#Graph .dropdown .dropdown-menu li").click(function(req,res) {
+	$("#Graph .dropdown .btn").text($(this).text());
+});
+
+
+
