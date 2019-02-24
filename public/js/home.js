5b9f2911e0f6f5c3c18ed0176afb67e4d1dceb94
Andrew Muro <muro_andrew@yahoo.com>
2019-02-23 16:55:10 -0800

    slight work on index page

--- public/js/home.js
+++ public/js/home.js
@@ -1,3 +1,54 @@
+// vars for time
+var mins = 0;
+var hrs = 0;
+
+$("#startButton").click(function(e){
+	e.preventDefault();
+	var name = $("#inAct").val();
+	var hrs = $("#hours").val();
+	var mins = $("#mins").val();	
+	//max time check
+	/*if(hrs=="24"){
+		var zeroMinutes = '<p>00</p>';
+		$(.html(zeroMinutes);
+	}
+	else{*/
+		$.get("/home/" + name + "/" + hrs + "/" + mins, redirect);
+	//}
+});
+
+function redirect(result){
+	window.location.href = "/start";
+}
+
+//24 hour time limit
+$("#hours").change(function(){
+	console.log($(this).val());
+	if($(this).val()==24){
+		console.log("here");
+		var zeroMinutes = '<option value="00">00</option>';
+		console.log(zeroMinutes);
+		$("#mins").html(zeroMinutes);
+	}
+	else{
+		var normalMinutes = '<option value="00">00</option>'+
+						'<option value="05">05</option>'+
+						'<option value="10">10</option>'+
+						'<option value="15">15</option>'+
+						'<option value="20">20</option>'+
+						'<option value="25">25</option>'+
+						'<option value="30">30</option>'+
+						'<option value="35">35</option>'+
+						'<option value="40">40</option>'+
+						'<option value="45">45</option>'+
+						'<option value="50">50</option>'+
+						'<option value="55">55</option>';
+		$("#mins").html(normalMinutes);
+		}
+});
+
+
+
 /*var newAct = require("../public/activities.json");
 
 $("#Start #startButton").click(function(e){
@@ -8,25 +59,6 @@ $("#Start #startButton").click(function(e){
 	console.log(newActivity)
 	newAct.activity.push(newActivity);
 });*/
-
-
-// vars for time
-var mins = 0;
-var hrs = 0;
-
-$("#startButton").click(function(e){
-	e.preventDefault();
-	var name = $("#inAct").val();
-	var hrs = $("#hours").val();
-	var mins = $("#mins").val();
-	console.log(name);
-	$.get("/home/" + name + "/" + hrs + "/" + mins, redirect);
-});
-
-function redirect(result){
-	window.location.href = "/start";
-} 
-
 /*
 $("#l1").click({num:1}, dec);
 $("#l2").click({num:2}, dec);
