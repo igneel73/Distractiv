9ffbec27c3b3741cdc011f2cd8a39718273d0aee
Andrew Muro <muro_andrew@yahoo.com>
2019-02-25 14:47:28 -0800

    more error messages

--- public/js/login.js
+++ public/js/login.js
@@ -4,30 +4,50 @@ $(document).ready(function(e){
 	$(".signup").hide();
 });
 
+//on login page, click sign up to show sign up page
 $(".signup-btn").click(function(e){
 	e.preventDefault();
 	$(".login").hide();
 	$(".signup").show();
 });
 
+//on create account/sign up page, click to go back to login
 $(".login-btn").click(function(e){
 	e.preventDefault();
 	$(".signup").hide();
 	$(".login").show();
 });
 
+
+
+//on login page
 $("#log").click(function(e){
 	e.preventDefault();
 	var name = $("#log-name").val();
-	console.log(name);
-	$.get("/" + name, redirect);
+	var pass = $("#log-pass").val();
+	//error modal
+	if(name=="" | pass==""){
+		$("#log").attr('data-target', "#logAndSignReq");
+	}
+	else{
+		$.get("/" + name, redirect);
+	}
 });
 
+
+//on create account/sign up page, click sign up to sign up, goes to start activity
 $("#sig").click(function(e){
 	e.preventDefault();
+	var email = $("#inputEmail").val();
 	var name = $("#sig-name").val();
-	console.log(name);
-	$.get("/" + name, redirect);
+	var pass= $("#passWord").val();
+	//error modal
+	if(email=="" | name=="" | pass==""){
+		$("#sig").attr('data-target', "#logAndSignReq");
+	}
+	else{
+		$.get("/" + name, redirect);
+	}
 });
 
 function redirect(result){
