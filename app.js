aa315eb7af190dc012569d9befecb5f2397b8daa
Andrew Muro <muro_andrew@yahoo.com>
2019-02-25 21:04:32 -0800

    some data

--- app.js
+++ app.js
@@ -42,6 +42,9 @@ app.get('/', login.view);
 app.get('/home', index.view);
 app.get('/start', progress.view);
 app.get('/data', data.view);
+//alternate for user testing
+app.get('/data/viewAlt', data.viewAlt);
+
 app.get('/:name', login.log);
 app.get('/home/:name/:hrs/:mins', index.next);
 app.get('/save/:dist', progress.save);
--- routes/data.js
+++ routes/data.js
@@ -4,7 +4,16 @@
 var act = require('../public/activities.json');
 
 exports.view = function(req, res){
-		res.render('data', act);
+	act['viewAlt'] = false;
+	console.log(act);
+	console.log(act['viewAlt']);
+	res.render('data', act);
 
+};
+
+exports.viewAlt = function(request, response){
+	act['viewAlt'] = true;
+	console.log(act);
+  	response.render('data', act);
 };
 
